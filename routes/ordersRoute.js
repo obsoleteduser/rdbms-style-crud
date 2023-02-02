706654b265ea78c0510d42aa6bdfe5aad4c66574
Tahir Dibirov II <tahirdibirovresearch@gmail.com>
2023-02-02 18:04:12 +0400

    INIT

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -3,7 +3,4 @@ import ordersController from '../controller/ordersController'
 
 export const ordersRoute = express.Router()
 
-ordersRoute.get('/orders', (req, res)=>{res.send("Hi from Orders")})
 ordersRoute.post('/orders', ordersController.addProduct)
-// ordersRoute.put()
-// ordersRoute.delete()
