b8af1933431b7b3fcef3338a6b1a4677693d4f57
Tahir Dibirov II <tahirdibirovresearch@gmail.com>
2023-02-02 17:54:34 +0400

    INIT

--- controller/ordersController.js
+++ controller/ordersController.js
@@ -1,13 +1,29 @@
-import { order } from "../models/ordersSchema"
+import { order } from "../models/ordersSchema";
 
-class OrdersController{
-    addProduct = async(req, res)=>{
-        try{
-           await order.create()
-        }catch(err){
-            res.send("Something went wrong!")
-        }
+class OrdersController {
+  addProduct = async (req, res) => {
+    try {
+      const {
+        productName,
+        categoryId,
+        productPrice,
+        productDescription,
+        buyerId,
+        orderDate,
+      } = req.body;
+     const newOrder = await order.create({
+        productName,
+        categoryId,
+        productPrice,
+        productDescription,
+        buyerId,
+        orderDate,
+      });
+      es.json(newOrder)
+    } catch (err) {
+      res.json(err);
     }
+  };
 }
 
-export default new OrdersController()
+export default new OrdersController();
